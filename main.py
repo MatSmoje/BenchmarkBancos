3276eb9b29e234589a27cdc4f270f7f3e2f96550
Matias Smoje <matias.smoje@gmail.com>
2022-12-10 12:16:48 -0300

    Processing B1

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def readData():
 
 
     for file in files: 
+        parquetName = file.split(sep='.')[0]
         if 'MB1' in file: 
             skiprow = 4
             skipfoot = 10
@@ -24,26 +25,27 @@ def readData():
             skiprow = 3
             skipfoot = 10
 
-        if 'MC1' in file:
-            print(f"Procesando {file}")
-            path = f"201907-280819/Instrucciones/{file}"
-            df = pd.read_csv(path, encoding = "ISO-8859-1", on_bad_lines='skip', sep='\t', skipfooter=skipfoot, skiprows=skiprow, header= None)
-            df.columns = ['Codigo','Descripcion']
-            df['Codigo'] = df['Codigo'].astype('str').apply(lambda x: x.lstrip())
-            df['Descripcion'] = df['Descripcion'].astype('str').apply(lambda x: x.lstrip().strip())
+        #if 'MC1' in file:
+        print(f"Procesando {file}")
+        path = f"201907-280819/Instrucciones/{file}"
+        df = pd.read_csv(path, encoding = "ISO-8859-1", on_bad_lines='skip', sep='\t', skipfooter=skipfoot, skiprows=skiprow, header= None)
+        df.columns = ['Codigo','Descripcion']
+        df['Codigo'] = df['Codigo'].astype('str').apply(lambda x: x.lstrip())
+        df['Descripcion'] = df['Descripcion'].astype('str').apply(lambda x: x.lstrip().strip())
 
-            for idx, row in df.iterrows():
-                if len(row['Codigo']) > 7: 
-                    val = row[0].split(sep=' ')
-                    df.at[idx,'Codigo'] = val[0]
-                    df.at[idx,'Descripcion'] = ' '.join(val[1:])
+        for idx, row in df.iterrows():
+            if len(row['Codigo']) > 7: 
+                val = row[0].split(sep=' ')
+                df.at[idx,'Codigo'] = val[0]
+                df.at[idx,'Descripcion'] = ' '.join(val[1:])
 
 
             #df[1] = df[1].apply(lambda x: x.strip())
             
-            print(df)
+        print(df)
+
             #print('----')
-        #df.to_parquet(f"{file}.parquet")
+        df.to_parquet(f"{parquetName}.parquet")
 
 
 readData()
